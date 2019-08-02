4ef568fc4ff24a1929b19bc397ded0c268ac905d
mdaskalov <thefallenangel.bg@gmail.com>
2019-08-02 17:59:05 +0800

    Lesson 18 - Laravel and Vue working together

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card" v-for="skill in skills">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        {{ skill }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                skills: []
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+
+            axios.get('/skills').then(response => this.skills = response.data);
+            // console.log(axios.response);
+        }
+    }
+</script>
